abe0b86fba778a801b1d21f979158cf739b50c8b
Kye-NG <kyenormangill98@gmail.com>
2021-04-25 16:16:17 +1000

    [HUD] use current player ped

--- client.lua
+++ client.lua
@@ -6,13 +6,13 @@ end)
 
 RegisterNetEvent('dmg')
 AddEventHandler('dmg', function(arg)
-  local ent = GetPlayerPed(-1)
+  local ent = currentPlayerPedId
   SetEntityHealth(ent, GetEntityHealth(ent) - 10)
 end)
 
 RegisterNetEvent('heal')
 AddEventHandler('heal', function(arg)
-  local ent = GetPlayerPed(-1)
+  local ent = currentPlayerPedId
   local currentHealth = GetEntityHealth(ent);
 
   if currentHealth < 200 then
@@ -22,7 +22,7 @@ end)
 
 RegisterNetEvent('armour')
 AddEventHandler('armour', function(arg)
-  local ent = GetPlayerPed(-1)
+  local ent = currentPlayerPedId
   SetPedArmour(ent, 100)
 end)
 
